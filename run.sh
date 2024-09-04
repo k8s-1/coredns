fc1fd7f58e15181f8ef92c3b538f5690fd17f7f9
pman <pman-$-432894328923@gmail.com>
2024-09-04 17:55:30 +0200

    ...

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker run -d --name coredns --restart=always --volume="$(pwd)":/root/ -p 53:53/udp coredns/coredns -conf /root/Corefile
+
+# query our server from a client in the same subnet or from the server directly
+LOCAL_IP=$(hostname -i | cut -d' ' -f1)
+dig @"$LOCAL_IP" host.example.com
